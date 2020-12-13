f20ad1b0ab5b63e07b732378c47009a5dd90cd98
brennaMacQuarrie <brennamacqurrie@gmail.com>
2020-12-13 16:37:14 -0700

    button casing

--- src/components/button.js
+++
@@ -1,7 +0,0 @@
-export default function Button(props) {
-    return (
-        <div className="submitDiv">
-            <button className="submitButton" onClick={props.addEntry} type="button" >send entry</button>
-        </div>
-    )
-}
